1f4211df52e341ce2093acdc718e8ccfa447d9b4
gabrieldasilva <josedasilva1999@outlook.es>
2024-01-16 22:40:35 -0300

    Ultimo commit desafio1

--- Desafio-1/ProductManager.js
+++ Desafio-1/ProductManager.js
@@ -1,31 +1,37 @@
 class ProductManager {
     constructor() {
         this.productos = [];
-        this.code = 1;
+        this.id = 1;
     }
 
     // Métodos
-    addProduct(title, description, price, thumbail, stock) {
+    addProduct(title, description, price, thumbail, code, stock) {
         
         const newProduct = {
-            id: this.code,
+            id: this.id,
             title,
             description,
             price,
             thumbail,
+            code,
             stock
         };
 
         
-        this.productos.push(newProduct);
-
-        
-        this.code++;
-
-        
         if (title == 0||description == 0 ||price == 0||stock == 0|| thumbail == 0) {
             console.log("No pueden existir datos vacíos");
+            return;
         }
+
+      
+        if (this.productos.some(product => product.code === code)) {
+            console.log(`El producto con el codigo ${code} ya existe`);
+            return;
+        }
+
+        this.productos.push(newProduct);
+        this.id++;
+
     }
 
     getProducts() {
@@ -50,15 +56,16 @@ const nuevoProducto = new ProductManager();
 
 
 
-nuevoProducto.addProduct("Arroz", "Blanco y rico", "$200", "./Multimedia/imagenes/arroz.png", 10);
+nuevoProducto.addProduct("Arroz", "Blanco y rico", "$200", "./Multimedia/imagenes/arroz.png", "abc1", 10);
 
-nuevoProducto.addProduct("Fideo", "El mejor fideo", "$300", "./Multimedia/imagenes/fideo.png", 10)
+
+nuevoProducto.addProduct("Arroz", "Blanco y rico", "$200", "./Multimedia/imagenes/arroz.png", "abc1" ,10);
 
 
 const todosLosProductos = nuevoProducto.getProducts();
 console.log(todosLosProductos);
 
 console.log("El id buscado:");
-console.log(nuevoProducto.getProductById(3));
+console.log(nuevoProducto.getProductById(4));
 console.log("El id buscado:")
 console.log(nuevoProducto.getProductById(2));
